8f167c4e7b36fcb2098a13a62f69aef44dfaae74
luoziqi <luoziqi@sarad-china.com>
2024-05-13 16:02:06 +0800

    chore: eslint+prettier+airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,32 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    'airbnb-base',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.*.json'
+      }
+    }
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'vue/multi-word-component-names': 1,
+    'no-unused-vars': 1,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
   }
 }
